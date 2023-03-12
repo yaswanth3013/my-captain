a9a4bdf3ba7de34b27e66fad78b03a7ee728c77b
kadali yaswanth kumar <72385909+yaswanth3013@users.noreply.github.com>
2023-03-12 11:15:41 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -5,9 +5,6 @@ r = float(input ("Enter radius of circle : "))
 print ("Area of the circle is: " + str(pi * r**2))
 
 """
-Output : 
-
-Enter radius of the circle : 6 
-
-Area of the circle is: 113.09733552923255
+input: Enter radius of the circle : 6 
+Output : Area of the circle is: 113.09733552923255
 """
