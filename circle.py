55330a31d5c2aba37abe2b2bd7c21be84be97ca2
kadali yaswanth kumar <72385909+yaswanth3013@users.noreply.github.com>
2023-03-12 10:55:29 +0530

    circle.py created by yaswanth

---
+++ circle.py
@@ -0,0 +1,13 @@
+from math import pi
+
+r = float(input ("Enter radius of circle : "))
+
+print ("Area of the circle is: " + str(pi * r**2))
+
+"""
+Output : 
+
+Enter radius of the circle : 6 
+
+Area of the circle is: 113.09733552923255
+"""
